48bafe64e0184f63915a705fd8bb18d8c83095ac
Ranjan Purbey <ranjan.purbey@gmail.com>
2019-12-27 18:00:49 +0530

    Use SSL for dev server

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import fs from 'fs';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,7 +43,13 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload({
+			watch: 'public',
+			https: {
+				key: fs.readFileSync('.ssl/key.pem'),
+				cert: fs.readFileSync('.ssl/cert.pem')
+			}
+		}),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
